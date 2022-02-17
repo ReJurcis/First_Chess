454b211f772da7d92d5db40c745984d1358d0fa0
ReJurcis <99201217+ReJurcis@users.noreply.github.com>
2022-02-17 21:21:23 +0100

    Initial commit

---
+++ src/com/company/Chess.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Chess {
+    private JTextField textField1;
+    private JButton okButton;
+    private JButton cancelButton;
+    private JPasswordField passwordField1;
+}
